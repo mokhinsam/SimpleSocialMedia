3c281e860a623246895ed199cd7213dbd4110526
Darina Samokhina <mokhinsam@gmail.com>
2025-11-17 16:42:35 +0300

    Initial commit

---
+++ SimpleSocialMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleSocialMedia
+//
+//  Created by Дарина Самохина on 17.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
