a142420712bc62fa3f3dfd2f686f8c8edf6ef7dc
Darina Samokhina <mokhinsam@gmail.com>
2025-11-19 14:34:46 +0300

    Added dir Models. Added model Post.

---
+++ SimpleSocialMedia/Models/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  SimpleSocialMedia
+//
+//  Created by Дарина Самохина on 19.11.2025.
+//
+
+struct Post: Decodable {
+    let userId: Int
+    let title: String
+    let body: String
+}
