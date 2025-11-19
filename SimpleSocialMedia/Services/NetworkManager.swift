453d4ca7499c2cf3f826756933e457f316625fb5
Darina Samokhina <mokhinsam@gmail.com>
2025-11-19 16:32:40 +0300

    Reinstall Alamofire to fix bug. Added Services dir, added singletone NetworkManager.

---
+++ SimpleSocialMedia/Services/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  SimpleSocialMedia
+//
+//  Created by Дарина Самохина on 19.11.2025.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+}
