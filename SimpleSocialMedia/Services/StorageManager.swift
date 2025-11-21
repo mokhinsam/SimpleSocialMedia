48d20ea8886187fcc22bd2ad4bedee8908bb178a
Darina Samokhina <mokhinsam@gmail.com>
2025-11-21 13:21:35 +0300

    Added StorageManager with initial settings. Deleted CoreData in AppDelegate, changed sceneDidEnterBackground in SceneDelegate.

---
+++ SimpleSocialMedia/Services/StorageManager.swift
@@ -0,0 +1,42 @@
+//
+//  StorageManager.swift
+//  SimpleSocialMedia
+//
+//  Created by Дарина Самохина on 21.11.2025.
+//
+
+import CoreData
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    // MARK: - Core Data stack
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "SimpleSocialMedia")
+        container.loadPersistentStores(completionHandler: { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private let viewContext: NSManagedObjectContext
+    
+    private init() {
+        viewContext = persistentContainer.viewContext
+    }
+    
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}
